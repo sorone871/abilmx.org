c936d6565febb2366d2dc6d2926411367d9edde9
Sorone <joseluis_rgoytia@hotmail.com>
2024-09-01 19:24:32 -0600

    slider

---
+++ src/js/app.js
@@ -0,0 +1,15 @@
+function cambiarFondo(bg, title) {
+    const banner = document.querySelector('.banner');
+    const contents = document.querySelectorAll('.content');
+
+    banner.style.background = `url("'../img/movies/${bg}")`;
+    banner.style.backgroundSize = 'cover';
+    banner.style.backgroundPosition = 'center';
+
+    contents.forEach(content =>{
+        content.classList.remove('active');
+        if (content.classList.contains(title)) {
+            content.classList.add('active');
+        }
+    });
+}
