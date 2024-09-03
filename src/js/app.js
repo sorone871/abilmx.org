4db42ce32765fd082054ea82c079d27b61fe2755
Sorone <joseluis_rgoytia@hotmail.com>
2024-09-02 22:23:07 -0600

    index

--- src/js/app.js
+++ src/js/app.js
@@ -2,10 +2,10 @@ function cambiarFondo(bg, title) {
     const banner = document.querySelector('.banner');
     const contents = document.querySelectorAll('.content');
 
-    banner.style.background = `url("'../img/movies/${bg}")`;
+    banner.style.background = `url("./build/img/movies/${bg}")`;
     banner.style.backgroundSize = 'cover';
     banner.style.backgroundPosition = 'center';
-
+    //http://127.0.0.1:5500/build/img/movies/bg-little-mermaid.jpg
     contents.forEach(content =>{
         content.classList.remove('active');
         if (content.classList.contains(title)) {
